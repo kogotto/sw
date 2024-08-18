6a376d96addd438edef00e644a28eb5ddb09092d
Yakov Didenko <kogotto@gmail.com>
2024-08-18 20:38:54 +0400

    Add usefull build scripts

---
+++ gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BUILD_DIR="build"
+OPTIONS="
+-DCMAKE_EXPORT_COMPILE_COMMANDS=1
+-DCMAKE_C_COMPILER=clang-15
+-DCMAKE_CXX_COMPILER=clang++-15
+-DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+-DCMAKE_BUILD_TYPE=Debug
+"
+
+mkdir $BUILD_DIR;
+
+cmake -S . -B $BUILD_DIR $OPTIONS;
+
+ln -sf $BUILD_DIR/compile_commands.json .;
